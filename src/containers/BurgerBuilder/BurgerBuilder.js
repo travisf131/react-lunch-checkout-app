e2f63b5070d18b085edf991cd3e0f63dd0351576
Travis <travisf131@gmail.com>
2021-01-16 13:43:07 -0500

    Added dynamic pricing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+import styled from 'styled-components';
+
+const PRICES = {
+    lettuce: 1.00,
+    cheese: 2.00,
+    meat: 4.50,
+    bacon: 3.25
+}
+const BASE_PRICE = 4.50
+
+const H3 = styled.h3 `
+    display: flex;
+    justify-content: center;
+    text-align: center;
+ `
 
 class BurgerBuilder extends Component {
 
@@ -10,25 +25,43 @@ class BurgerBuilder extends Component {
             cheese: 0,
             bacon: 0,
             meat: 0
-        }
+        },
+        totalPrice: BASE_PRICE
     }
 
+    // increase ingredient amount and total price
     increaseAmountButton = (ingName) => {
-        let ingredientsCopy = this.state.ingredients
-        ingredientsCopy[ingName] += 1
+        let ingredientAmount = this.state.ingredients
+        ingredientAmount[ingName] += 1
+
+        const oldPrice = this.state.totalPrice
+        let newTotal = oldPrice + PRICES[ingName]
+
         this.setState({
-            ingredients: ingredientsCopy
+            ingredients: ingredientAmount,
+            totalPrice: newTotal
         })
     }
 
+    // decrease ingredient amount and total price
     decreaseAmountButton = (ingName) => {
-        let ingredientsCopy = this.state.ingredients
-        
-        if( ingredientsCopy[ingName] > 0 ) {
-            ingredientsCopy[ingName] -= 1
-        }
+        let ingredientAmount = this.state.ingredients
+        const oldPrice = this.state.totalPrice
+
+        // keep the new total the same as the old one by defualt and only update if
+        // the condition below is met (there are > 0 ingredients)
+        let newTotal = oldPrice 
+
+        if (ingredientAmount[ingName] > 0 ) {
+            ingredientAmount[ingName] -= 1
+
+            //change price only if there is an ingredient to remove
+            newTotal = oldPrice - PRICES[ingName]   
+        } 
+
         this.setState({
-            ingredients: ingredientsCopy
+            ingredients: ingredientAmount,
+            totalPrice: newTotal
         })
     }
 
@@ -36,6 +69,7 @@ class BurgerBuilder extends Component {
         return (
             <div>
                 <Burger ingredients={this.state.ingredients} />
+                <H3>Total Price: ${this.state.totalPrice}</H3>
                 <BuildControls 
                     clickLess={this.decreaseAmountButton} 
                     clickMore={this.increaseAmountButton}/>
